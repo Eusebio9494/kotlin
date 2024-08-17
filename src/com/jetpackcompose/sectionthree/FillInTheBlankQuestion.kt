70b5fdb22f86adece5a1b2809652a8db8c43ade5
Eusebio <eusebio_olvera9494@hotmail.com>
2024-08-17 14:51:04 -0600

    Se convierte clase en una clase de datos-solo contienen datos
    Una clase de datos tampoco puede ser abstract, open, sealed ni inner.

--- src/com/jetpackcompose/sectionthree/FillInTheBlankQuestion.kt
+++ src/com/jetpackcompose/sectionthree/FillInTheBlankQuestion.kt
@@ -3,7 +3,7 @@ package com.jetpackcompose.sectionthree
 /**
  * Reutilizables Genericos
  */
-class Question<T>(
+data class Question<T>(
     val questionText: String,
     val answer: T,
     val difficulty: Difficulty
@@ -17,4 +17,6 @@ fun main() {
     val question1 = Question<String>("Quoth the raven ___", "nevermore", Difficulty.EASY)
     val question2 = Question<Boolean>("The sky is green. True or false", false, Difficulty.MEDIUM)
     val question3 = Question<Int>("How many days are there between full moons?", 28, Difficulty.HARD)
+
+    println(question1.toString())
 }
