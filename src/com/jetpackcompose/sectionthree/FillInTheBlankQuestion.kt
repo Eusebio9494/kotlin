77e267136fa937ce678ad204405c376c5a14fa6d
Eusebio <eusebio_olvera9494@hotmail.com>
2024-08-19 13:36:24 -0600

    Se reescriben funciones de extensión con interfaces

--- src/com/jetpackcompose/sectionthree/FillInTheBlankQuestion.kt
+++ src/com/jetpackcompose/sectionthree/FillInTheBlankQuestion.kt
@@ -13,7 +13,26 @@ enum class Difficulty {
     EASY, MEDIUM, HARD
 }
 
-class Quiz {
+interface ProgressPrintable {
+    val progressText: String
+
+    fun printProgressBar()
+}
+
+
+class Quiz : ProgressPrintable {
+
+    override val progressText: String
+        get() = "${answered} of ${total} answered"
+
+    override fun printProgressBar() {
+        repeat(answered) { print("▓") }
+        repeat(total - answered) { print("▒") }
+        println()
+        println(progressText)
+    }
+
+
     val question1 = Question<String>("Quoth the raven ___", "nevermore", Difficulty.EASY)
     val question2 = Question<Boolean>("The sky is green. True or false", false, Difficulty.MEDIUM)
     val question3 = Question<Int>("How many days are there between full moons?", 28, Difficulty.HARD)
@@ -24,6 +43,26 @@ class Quiz {
     }
 }
 
+
 fun main() {
-    print("${Quiz.answered} of ${Quiz.total} answered")
+
+    //print("${Quiz.answered} of ${Quiz.total} answered")
+    //println(Quiz.progressText)
+    Quiz().printProgressBar()
+
 }
+
+/*
+val Quiz.StudentProgress.progressText: String
+    get() = "${answered} of ${total} answered"
+*/
+
+/*
+fun Quiz.StudentProgress.printProgressBar() {
+    repeat(Quiz.answered) { print("▓") }
+    repeat(Quiz.total - Quiz.answered) { print("▒") }
+    println()
+    println(Quiz.progressText)
+}
+*/
+
